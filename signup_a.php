e58b34eb9290a6480cf1b307c3d4ab65a470bc2e
suraj setty <suraj716jh@gmail.com>
2022-05-07 12:04:50 +0530

    style

--- signup_a.php
+++ signup_a.php
@@ -11,7 +11,7 @@ else if(isset($_POST['save']))
 {
     
  
- 
+
         $query="INSERT INTO register(First_Name, Last_Name, Email, Password) VALUES ('$first_name', '$last_name', '$email', 'md5($pass)')";
         $sql=mysqli_query($conn,$query)or die("Could Not Perform the Query");
         header ("Location: signin.php?status=success");
